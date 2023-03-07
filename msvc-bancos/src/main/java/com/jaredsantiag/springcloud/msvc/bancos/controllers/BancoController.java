1b3e33bc02974bb0b1c631748723a9dddeb04417
Jared Santiago Garcia <jaredarturo105@gmail.com>
2023-03-06 20:39:35 -0600

    API Bancos

--- msvc-bancos/src/main/java/com/jaredsantiag/springcloud/msvc/bancos/controllers/BancoController.java
+++ msvc-bancos/src/main/java/com/jaredsantiag/springcloud/msvc/bancos/controllers/BancoController.java
@@ -3,13 +3,16 @@ package com.jaredsantiag.springcloud.msvc.bancos.controllers;
 import com.jaredsantiag.springcloud.msvc.bancos.entity.Banco;
 import com.jaredsantiag.springcloud.msvc.bancos.services.BancoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Controller
 public class BancoController {
     @Autowired
     private BancoService bancoService;
@@ -25,6 +28,36 @@ public class BancoController {
         if (optional.isPresent()) {
             return ResponseEntity.ok(optional.get());
         }
+        System.out.println("Here 1");
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> crear(@RequestBody Banco banco){
+        Banco bancoDB = bancoService.guardar(banco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bancoDB);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Banco banco, @PathVariable Long id){
+        Optional<Banco> bancoOptional = bancoService.porId(id);
+        if(bancoOptional.isPresent()){
+            Banco bancoDB = bancoOptional.get();
+            bancoDB.setNombre(banco.getNombre());
+            return ResponseEntity.status(HttpStatus.CREATED).body(bancoService.guardar(bancoDB));
+        }
+        System.out.println("Here 2");
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Banco> bancoOptional = bancoService.porId(id);
+        if(bancoOptional.isPresent()){
+            bancoService.eliminar(bancoOptional.get().getId());
+            return ResponseEntity.noContent().build();
+        }
+        System.out.println("Here 3");
         return ResponseEntity.notFound().build();
     }
 }
