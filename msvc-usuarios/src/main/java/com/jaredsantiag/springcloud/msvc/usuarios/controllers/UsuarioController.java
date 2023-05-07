7a75c0b0b72650a7742d1b9295e216055a198e16
Jared Santiago Garcia <jaredarturo105@gmail.com>
2023-05-06 21:38:01 -0600

    Kubernetes

--- msvc-usuarios/src/main/java/com/jaredsantiag/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/jaredsantiag/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -3,6 +3,8 @@ package com.jaredsantiag.springcloud.msvc.usuarios.controllers;
 import com.jaredsantiag.springcloud.msvc.usuarios.moles.entity.Usuario;
 import com.jaredsantiag.springcloud.msvc.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,6 +18,14 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/crash")
+    public void crash(){
+        ((ConfigurableApplicationContext) context).close();
+    }
 
     @GetMapping("/")
     public List<Usuario> listar(){
